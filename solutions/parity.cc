d4ecce7d4ccac64e010596f0edda4ae28c6606a4
Paul Foo <paul.foo@polymathian.com>
2019-04-20 00:35:33 +1000

    update parity

--- solutions/parity.cc
+++ solutions/parity.cc
@@ -3,13 +3,13 @@
 
 short brute_force_parity(unsigned long long x)
 {
-  short count = 0;
-  while (x)
-  {
-      count = count + (x & 1);
-      x = x >> 1;
-  }
-  return x % 2;
+    short result = 0;
+    while (x)
+    {
+        result ^= x & 1;
+        x = x >> 1;
+    }
+    return result;
 }
 
 short improved_parity(unsigned long long x)
@@ -34,10 +34,6 @@ short optimal_parity(unsigned long long x)
     return x & 1;
 }
 
-short Parity(unsigned long long x)
-{
-    return 0;
-}
 
 int main(int argc, char* argv[]) {
     return 0;
