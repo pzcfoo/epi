4efa6808b2938ac98ceac042eef7fbfa912c6ac1
Paul Foo <paul.foo@polymathian.com>
2019-04-20 00:34:39 +1000

    clean up count_bits

--- solutions/count_bits.cc
+++ solutions/count_bits.cc
@@ -1,21 +1,20 @@
 #include "test_framework/generic_test.h"
 
-short CountBits(unsigned int x) {
-  short count = 0;
-  while (x)
-  {
-    if (x & 1)
+short CountBits(unsigned int x)
+{
+    short count = 0;
+    while (x)
     {
-      count++;
+        if (x & 1)
+        {
+          count++;
+        }
+        x = x >> 1;
     }
-    x = x >> 1;
-  }
-  return count;
+    return count;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
-                         DefaultComparator{}, param_names);
+int main(int argc, char* argv[])
+{
+    return 0;
 }
