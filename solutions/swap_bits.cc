9e65b191035a969f363bdcc88cac2a0887f5571f
Paul Foo <paul.foo@polymathian.com>
2019-04-20 00:52:31 +1000

    optimal swap bits

--- solutions/swap_bits.cc
+++ solutions/swap_bits.cc
@@ -1,3 +1,20 @@
+long long optimal_swap_bits(long long x, int i, int j)
+{
+    short val_i = (x >> i) & 1;
+    short val_j = (x >> j) & 1;
+    if (val_i == val_j)
+    {
+        return x;
+    }
+    else
+    {
+        long long mask = (1LL << i) | (1LL << j);
+        x ^= mask;
+    }
+    return x;
+}
+
+
 long long swap_bits(long long x, int i, int j)  
 {                     
     short val_i = (x >> i) & 1;                  
@@ -25,4 +42,5 @@ long long swap_bits(long long x, int i, int j)
         x &= mask_i;       
     }                      
     return x;              
-}   
+}
+
