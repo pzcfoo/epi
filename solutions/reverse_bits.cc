1d304281e3d748c83238d7d63ccdd17e6f1cecdb
Paul Foo <paul.foo@polymathian.com>
2019-04-21 00:02:55 +1000

    reverse bits with xor

--- solutions/reverse_bits.cc
+++ solutions/reverse_bits.cc
@@ -2,7 +2,7 @@
 #include <bitset>
 
 
-unsigned long long ReverseBits(unsigned long long x) 
+unsigned long long BruteForceReverseBits(unsigned long long x) 
 {
     unsigned long long ans = 0;
     int l = 63; 
@@ -26,20 +26,34 @@ unsigned long long ReverseBits(unsigned long long x)
     return ans;
 }
 
+// slightly better
+unsigned long long ReverseBits(unsigned long long x)
+{
+    unsigned long long ans = x;
+    int l = 63;
+    for (int i=0; i < 32; i++)
+    {
+        int j = (l - i);
+        int val_i = (x >> i) & 1;
+        int val_j = (x >> j) & 1;
+        if (val_i != val_j)
+        {
+            unsigned long long mask = 1ULL << i;
+            mask |= (1ULL << j);
+            ans ^= mask;
+        }
+    }
+    return ans;
+}
+
+
 
 int main ()
 {
-    unsigned long long b = 4611686018427387904;
-    //unsigned long long test = 1351510410656;
-    std::cout << std::bitset<64>(b) << std::endl;
-    auto ans = ReverseBits(b);
+    unsigned long long test = 1351510410656;
+    std::cout << std::bitset<64>(test) << std::endl;
+    auto ans = ReverseBits(test);
     std::cout << std::bitset<64>(ans) << std::endl;
-    { 
-        //unsigned long long a = 10;
-        //std::cout << std::bitset<64>(a) << std::endl;
-        //auto ans = ReverseBits(a);
-        //std::cout << std::bitset<64>(ans) << std::endl;
-    }
     return 0;
 }
 
