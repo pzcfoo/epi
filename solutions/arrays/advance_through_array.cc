9a994b262e4d7573ec930d72bb83e2bdde04bcab
Paul Foo <paul.foo@polymathian.com>
2019-06-12 08:24:13 +1000

    organise dirs

--- solutions/arrays/advance_through_array.cc
+++ solutions/arrays/advance_through_array.cc
@@ -15,12 +15,10 @@ bool can_reach_end(const vector<int>& max_advance_steps)
     std::cout << ix_end << std::endl;
     for (int i = 0; i < max_advance_steps.size(); i++)
     {
-        std::cout << "i: " << i << std::endl;
         auto steps = max_advance_steps[i];
         for (int j=1; j <= steps; j++)
         {
             auto ix_next = j + i;
-            std::cout << "j: " << j << std::endl;
             if (ix_next == ix_end)
             {
                 return true;
@@ -36,28 +34,46 @@ bool can_reach_end(const vector<int>& max_advance_steps)
     return false;
 }
 
+// O(1) space instead of O(n)
+bool opt_can_reach_end(const vector<int>& max_advance_steps)
+{
+    if (max_advance_steps.size() == 1)
+    {
+        return true;
+    }
+    auto ix_end = max_advance_steps.size() - 1;
+    int max = 0; // max ix we can get to
+    for (int i = 0; (i < max_advance_steps.size()) && (max >= i); i++)
+    {
+        int current_max_ix = i + max_advance_steps[i];
+        max = max > current_max_ix ? max : current_max_ix;
+    }
+
+    return max >= ix_end;
+}
+
 
 int main()
 {
     {
         vector<int> a = {3,3,1,0,2,0,1};
-        auto ans = can_reach_end(a);
-        std::cout << ans << std::endl; 
+        auto ans = opt_can_reach_end(a);
+        std::cout << "case 0: " << ans << std::endl; 
     }
     {
-        vector<int> a = {0};
-        auto ans = can_reach_end(a);
-        std::cout << ans << std::endl; 
+      vector<int> a = {0};
+        auto ans = opt_can_reach_end(a);
+        std::cout << "case 1: " << ans << std::endl; 
     }
     {
         vector<int> a = {0, 0};
-        auto ans = can_reach_end(a);
-        std::cout << ans << std::endl; 
+        auto ans = opt_can_reach_end(a);
+        std::cout << "case 2: " << ans << std::endl; 
     }
     {
         vector<int> a = {1, 0, 0};
-        auto ans = can_reach_end(a);
-        std::cout << ans << std::endl; 
+        auto ans = opt_can_reach_end(a);
+        std::cout << "case 3: " << ans << std::endl; 
     }
 }
 
--- solutions/closest_int_same_weight.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream> 
-
-
-unsigned long long ClosestIntSameBitCount(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
-}
